0508cbf90e80ef22ff596f82d1ed1313f9165e8f
Ryan <ryancfreas@gmail.com>
2025-03-13 20:13:18 -0500

    Update reviewController.js

--- backEnd/controllers/reviewController.js
+++ backEnd/controllers/reviewController.js
@@ -21,7 +21,8 @@ const getReviews = async (req, res) => {
 const createReviews = async (req, res) => {
     try{
         const review = await Review.create(req.body);
-        res.status(200).json(review);
+        res.set('Content-Type', 'application/xml');
+        res.status(200).send(JSON.stringify(review));
     } catch( error) {
         throw error;
     }
